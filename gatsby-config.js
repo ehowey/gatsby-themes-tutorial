f4b072f636d727e4b46729bdce58b78598c9ee71
Eric Howey <ehowey@gmail.com>
2020-02-26 22:23:00 -0700

    Add gatsby-config metaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,35 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Multiple Themes`,
+    description: `A tutorial for building a Gatsby site using multiple themes.`,
+    author: `Your name`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/notes`,
+      },
+    },
+  ],
 }
